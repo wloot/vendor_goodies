8310913f84b6d5a62d8cb540b8747883495c3ba6
Vol Zhdanov <wight554@gmail.com>
2018-08-29 10:51:09 +0000

    MiuiCamera: guard chiron device config

--- config.mk
+++ config.mk
@@ -15,7 +15,6 @@
 VENDOR_PATH := vendor/MiuiCamera
 
 PRODUCT_COPY_FILES += \
-    $(VENDOR_PATH)/system/etc/device_features/chiron.xml:system/etc/device_features/chiron.xml \
     $(VENDOR_PATH)/system/etc/default-permissions/miuicamera-permissions.xml:system/etc/default-permissions/miuicamera-permissions.xml \
     $(VENDOR_PATH)/system/lib/libCameraEffectJNI.so:system/lib/libCameraEffectJNI.so \
     $(VENDOR_PATH)/system/lib/libblurbuster.so:system/lib/libblurbuster.so \
@@ -46,6 +45,11 @@ PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/system/lib64/libmorpho_memory_allocator.so:system/lib64/libmorpho_memory_allocator.so \
     $(VENDOR_PATH)/system/lib64/libmorpho_panorama.so:system/lib64/libmorpho_panorama.so \
     $(VENDOR_PATH)/system/lib64/libmorpho_panorama_gp.so:system/lib64/libmorpho_panorama_gp.so \
+
+ifeq ($(TARGET_DEVICE),chiron)
+PRODUCT_COPY_FILES += \
+    $(VENDOR_PATH)/system/etc/device_features/chiron.xml:system/etc/device_features/chiron.xml
+endif
 
 PRODUCT_PACKAGES += \
     MiuiCamera
